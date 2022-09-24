df9e3fefa423a78e8db828da06358744de3e72d6
Rena Salazar <renasalazart@outlook.com>
2022-09-23 20:47:14 -0600

    laravel ui installation and login and register views modification

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Auth;
 
 
 /*
@@ -20,3 +21,6 @@ use App\Http\Controllers\PostController;
 Route::get('/', [PagesController::class, 'index']);
 
 Route::resource('/blog', PostController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
